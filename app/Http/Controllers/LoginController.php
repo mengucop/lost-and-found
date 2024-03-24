fd853dbe6fc83cafc35b11313da0ecb88f813eb3
Andalib Rahman Shagoto <andalibrahman369@gmail.com>
2024-03-24 12:41:07 +0600

    Added routes, models, controllers

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Student;
+
+class LoginController extends Controller
+{
+    //
+    public function index()
+    {
+        return view('login');
+    }
+
+    public function login(Request $request)
+    {
+        $username = $request->input('username');
+        $password = $request->input('password');
+
+        $student = Student::where('username', $username)->first();
+
+        if ($student) 
+        {
+            if ($student->password == $password) 
+            {
+                $request->session()->put('student', $student);
+                return redirect()->route('home');
+            }
+        }
+        else
+        {
+            return redirect()->route('login');
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        $request->session()->forget('student');
+        return redirect()->route('home');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [LoginController::class, 'index'])->name('home');
+Route::post('/', [LoginController::class, 'login'])->name('login');
+
+Route::get('/register', [RegisterController::class, 'index'])->name('register');
+Route::post('/register', [RegisterController::class, 'register'])->name('register');
+
+Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
