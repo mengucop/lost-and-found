eabc8cb19eb677b38177421d55780d3cef3a7980
Andalib Rahman Shagoto <andalibrahman369@gmail.com>
2024-03-29 18:46:02 +0600

    Tailwind added

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,6 +1,18 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
+    <title>Login</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    @vite('resources/css/app.css')
 </head>
+<body>
+    <h1 class="underline">Hello world</h1>
+    <form action="{{ route('login') }}" method="post">
+        @csrf
+        <input class="border" type="email" name="email" placeholder="Email">
+        <input type="password" name="password" placeholder="Password">
+        <button type="submit">Login</button>
+    </form>
+</body>                
 </html>
